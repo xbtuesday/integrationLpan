e22ee2224a68439af6c77e7f4e5dc998a5f63a2f
Administrator <spn4718@163.com>
2017-06-01 17:14:57 +0800

    .。。

--- springRedis/src/main/java/com/Lpan/system/controller/IndexLoginController.java
+++
@@ -1,8 +0,0 @@
-package com.Lpan.system.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class IndexLoginController {
-
-}
--- springRedis/src/main/java/com/Lpan/system/controller/MemberController.java
+++
@@ -1,9 +0,0 @@
-package com.Lpan.system.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MemberController {
-	
-	
-}
